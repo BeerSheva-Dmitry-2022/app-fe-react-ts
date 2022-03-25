609ebc6dc1ec6d16e36c57346cf6d848f98b3de7
DimaTch <4dnik@mail.ru>
2022-03-25 19:44:15 +0300

    timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [status, setStatus] = useState("start");
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    if (status == "pause") {
+      setTimeout(() => {
+        setValue(value + 1)
+      }
+        , 1000);
+    }
+  }, [status, value]);
+
+  function handlerClick() {
+    const res = status == "start" ? "pause" : "start";
+    setStatus(res);
+  }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{value}</h1>
+      <button onClick={handlerClick}>{status}</button>
+
     </div>
   );
 }
